47ee503a2c7eae8c5e6af15d9683e9086ee3e47d
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-11 14:51:15 +0200

    More features

--- src/store/balance/slice.js
+++ src/store/balance/slice.js
@@ -13,11 +13,17 @@ export const balanceSlice = createSlice({
       // empty for now! :)
       state.amount = state.amount + action.payload;
     },
+    withdraw: (state, action) => {
+      state.amount = state.amount - action.payload;
+    },
+    reset: (state, action) => {
+      state.amount = initialState.amount;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
 // as we add cases to our reducer we will also export the corresponding actions
-export const { deposit } = balanceSlice.actions;
+export const { deposit, withdraw, reset } = balanceSlice.actions;
 
 export default balanceSlice.reducer;
